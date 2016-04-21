76c0fd26810c24d1d20250019700c82e5705a940
Ben Thompson <t.ben.thompson@gmail.com>
2016-04-21 00:26:06 -0400

    python installation seems to be working.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distutils.sysconfig import get_python_inc
 from pip import locations
 import multiprocessing
 import os
+import shutil
 import sys
 import setuptools
 import subprocess
@@ -32,20 +33,50 @@ def build_extension():
         '..'
     ])
     subprocess.call(['make', '-j' + str(n_cores)])
+    os.chdir(os.pardir)
+
+    libname = 'taskloaf_wrapper.so'
+    shutil.copy(
+        os.path.join('build_python', libname),
+        os.path.join('taskloaf', libname)
+    )
 
 def main():
     build_extension()
 
     setup(
-        name='taskloaf',
-        version='0.0.1',
-        author='T. Ben Thompson',
-        author_email='t.ben.thompson@gmail.com',
-        url='https://github.com/tbenthompson/taskloaf',
-        description='taskloaf',
-        long_description='',
-        install_requires=['dill'],
-        zip_safe=False,
+        packages = ['taskloaf'],
+        include_package_data = True,
+        package_data = {'taskloaf': ['taskloaf_wrapper.so']},
+        install_requires = ['dill'],
+        zip_safe = False,
+
+        name = 'taskloaf',
+        version = '0.0.1',
+        description = 'taskloaf',
+        long_description = '',
+        url = 'https://github.com/tbenthompson/taskloaf',
+        author = 'T. Ben Thompson',
+        author_email = 't.ben.thompson@gmail.com',
+        classifiers = [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'Operating System :: OS Independent',
+            'Operating System :: POSIX',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Software Development',
+            'Topic :: System :: Distributed Computing',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: C++'
+        ],
     )
 
 if __name__ == '__main__':
