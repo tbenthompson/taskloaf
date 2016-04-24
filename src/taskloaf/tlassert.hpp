6e4d221c5a43cf903224f598a7ecb8c1e4786b9a
Ben Thompson <t.ben.thompson@gmail.com>
2016-04-24 02:09:38 -0400

    Try again with MPI on travis

--- src/taskloaf/tlassert.hpp
+++ src/taskloaf/tlassert.hpp
@@ -4,7 +4,7 @@ namespace taskloaf {
 void _assert(const char* expression, const char* file, int line);
 }
  
-#ifndef NTLASSERT
+#ifdef TLASSERT
 #define tlassert(EXPRESSION) ((EXPRESSION) ? (void)0 : _assert(#EXPRESSION, __FILE__, __LINE__))
 #else
 #define tlassert(EXPRESSION) ((void)0)
