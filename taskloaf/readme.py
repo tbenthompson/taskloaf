f3ae1445631a3de2916c6d427d063345fcecbd38
Ben Thompson <t.ben.thompson@gmail.com>
2016-04-24 23:02:11 -0400

    Remove semicolon

--- taskloaf/readme.py
+++ taskloaf/readme.py
@@ -3,7 +3,7 @@ from operator import add
 
 def fib(index):
     if index < 3:
-        return tl.ready(1);
+        return tl.ready(1)
     else:
         return tl.when_all(fib(index - 1), fib(index - 2)).then(add)
 
