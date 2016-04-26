fa42ee5af81aa57e53732862fc386fb0cec3575f
Ben Thompson <t.ben.thompson@gmail.com>
2016-04-26 02:36:18 -0400

    Working towards cheap small tasks

--- src/taskloaf/future.hpp
+++ src/taskloaf/future.hpp
@@ -4,58 +4,117 @@
 #include "fnc.hpp"
 #include "execute.hpp"
 #include "closure.hpp"
+#include "worker.hpp"
 
 namespace taskloaf {
 
 template <typename... Ts> struct Future;
 
-IVarRef plan_then(const IVarRef& input, PureTaskT task);
-IVarRef plan_unwrap(const IVarRef& input, PureTaskT unwrapper);
-IVarRef plan_ready(Data data);
-IVarRef plan_async(PureTaskT task);
-IVarRef plan_when_all(std::vector<IVarRef> inputs);
-
 template <typename F, typename... Ts>
 auto then(const Future<Ts...>& fut, F&& fnc) {
     typedef typename std::result_of<F(Ts&...)>::type Return;
     auto fnc_container = make_function(std::forward<F>(fnc));
-    Closure<Data(std::vector<Data>&)> task(
-        [] (std::vector<Data>& d, std::vector<Data>& in) mutable
-        {
-            return make_data(apply_args(in, d[0].get_as<decltype(fnc_container)>()));
+    IVarRef out_future(new_id());
+    cur_worker->add_trigger(fut.ivar, {
+        [] (std::vector<Data>& d, std::vector<Data>& vals) {
+            cur_worker->add_task({
+                [] (std::vector<Data>& d) {
+                    auto& out_future = d[0].get_as<IVarRef>();
+                    auto& fnc = d[1].get_as<decltype(fnc_container)>();
+                    auto& vals = d[2].get_as<std::vector<Data>>();
+                    cur_worker->fulfill(out_future, {
+                        make_data(apply_args(vals, fnc))
+                    });
+                },
+                {d[0], d[1], make_data(vals)}
+            });
         },
-        {make_data(std::move(fnc_container))}
-    );
-    return Future<Return>{plan_then(fut.ivar, std::move(task))};
+        {make_data(out_future), make_data(std::move(fnc_container))}
+    });
+    return Future<Return>{out_future};
 }
 
+//TODO: unwrap doesn't actually need to do anything. It can be
+//lazy and wait for usage.
 template <typename T>
 auto unwrap(const Future<T>& fut) {
-    Closure<Data(std::vector<Data>&)> task(
-        [] (std::vector<Data>&, std::vector<Data>& in) {
-            return make_data(in[0].get_as<T>().ivar);
+    IVarRef out_future(new_id());
+    cur_worker->add_trigger(fut.ivar, {
+        [] (std::vector<Data>& d, std::vector<Data>& vals) {
+            auto result = vals[0].get_as<T>().ivar;
+            cur_worker->add_trigger(result, {
+                [] (std::vector<Data>& d, std::vector<Data>& vals) {
+                    cur_worker->fulfill(d[0].get_as<IVarRef>(), vals); 
+                },
+                {d[0]}
+            });
         },
-        {}
-    );
-    return Future<typename T::type>{plan_unwrap(fut.ivar, std::move(task))};
+        {make_data(out_future)}
+    });
+    return Future<typename T::type>{out_future};
 }
 
 template <typename T>
 auto ready(T val) {
-    return Future<T>{plan_ready(make_data(std::move(val)))};
+    IVarRef out_future(new_id());
+    cur_worker->fulfill(out_future, {make_data(std::move(val))});
+    return Future<T>{out_future};
 }
 
 template <typename F>
 auto async(F&& fnc) {
     typedef typename std::result_of<F()>::type Return;
     auto fnc_container = make_function(std::forward<F>(fnc));
-    Closure<Data(std::vector<Data>&)> task(
-        [] (std::vector<Data>& d, std::vector<Data>&) {
-            return make_data(d[0].get_as<decltype(fnc_container)>()());
+    IVarRef out_future(new_id());
+    cur_worker->add_task({
+        [] (std::vector<Data>& d) {
+            auto& out_future = d[0].get_as<IVarRef>();
+            auto& fnc = d[1].get_as<decltype(fnc_container)>(); 
+            cur_worker->fulfill(out_future, {make_data(fnc())});
         },
-        {make_data(std::move(fnc_container))}
-    );
-    return Future<Return>{plan_async(std::move(task))};
+        {make_data(out_future), make_data(std::move(fnc_container))}
+    });
+    return Future<Return>{out_future};
+}
+
+inline void whenall_child(std::vector<IVarRef> child_results, size_t next_idx,
+    std::vector<Data> accumulator, IVarRef result) 
+{
+    auto next_result = std::move(child_results[next_idx]);
+    if (next_idx == child_results.size() - 1) {
+        cur_worker->add_trigger(next_result, {
+            [] (std::vector<Data>& d, std::vector<Data>& vals) {
+                auto& result = d[0].get_as<IVarRef>();
+                auto& accumulator = d[1].get_as<std::vector<Data>>();
+                accumulator.push_back(vals[0]);
+                cur_worker->fulfill(result, std::move(accumulator));
+            },
+            { make_data(result), make_data(std::move(accumulator)) }
+        });
+    } else {
+        cur_worker->add_trigger(next_result, {
+            [] (std::vector<Data>& d, std::vector<Data>& vals) {
+                auto& child_results = d[0].get_as<std::vector<IVarRef>>();
+                auto& next_idx = d[1].get_as<size_t>();
+                auto& accumulator = d[2].get_as<std::vector<Data>>();
+                auto& result = d[3].get_as<IVarRef>();
+                accumulator.push_back(vals[0]);
+                whenall_child(std::move(child_results), next_idx + 1,
+                    std::move(accumulator), std::move(result)
+                );
+            },
+            {
+                make_data(std::move(child_results)), make_data(next_idx),
+                make_data(std::move(accumulator)), make_data(std::move(result)) 
+            }
+        });
+    }
+}
+
+IVarRef plan_when_all(std::vector<IVarRef> inputs) {
+    IVarRef out_future(new_id());
+    whenall_child(std::move(inputs), 0, {}, out_future);
+    return out_future;
 }
 
 template <typename... Ts>
@@ -63,6 +122,7 @@ auto when_all(const Future<Ts>&... args) {
     std::vector<IVarRef> data{args.ivar...};
     return Future<Ts...>{plan_when_all(std::move(data))};
 }
+
 
 template <typename... Ts>
 struct Future {
