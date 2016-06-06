a6c18ca9ea81048e74fa96e0843b13eb94158672
Ben Thompson <t.ben.thompson@gmail.com>
2016-06-06 14:40:54 -0400

    Using variant for data...

--- src/taskloaf/data.hpp
+++ src/taskloaf/data.hpp
@@ -1,6 +1,9 @@
 #pragma once
+#include "tlassert.hpp"
 
-#include "fnc.hpp"
+#include <cereal/archives/binary.hpp>
+
+#include <mapbox/variant.hpp>
 
 namespace taskloaf {
 
@@ -17,91 +20,46 @@ struct Data {
         "Types encapsulated in Data must be serializable."
     );
     
-    union {
-        T val;
-        std::shared_ptr<T> ptr;
-    };
-    int which = 0;
+    mapbox::util::variant<T,std::shared_ptr<T>> v;
 
-    Data(): val() {}
-    Data(const T& v): val(v) {}
-    Data(T&& v): val(std::move(v)) {}
+    Data() = default;
+    Data(const T& v): v(v) {}
+    Data(T&& v): v(std::move(v)) {}
 
-    Data(Data<T>&& other):
-        val()
-    {
-        *this = std::move(other);
-    }
+    Data(Data<T>&& other) = default;
+    Data& operator=(Data<T>&& other) = default;
 
-    Data& operator=(Data<T>&& other) {
-        this->~Data();    
-        which = other.which;
-        if (which == 0) {
-            new (&val) T(std::move(other.val));
-        } else {
-            new (&ptr) std::shared_ptr<T>(std::move(other.ptr));
-        }
-        return *this;
-    }
-
-    Data(const Data<T>& other) {
-        *this = other;
-    }
+    Data(const Data<T>& other) { *this = other; }
     Data& operator=(const Data<T>& other) {
         const_cast<Data<T>*>(&other)->promote();
-        this->~Data();
-        which = 1;
-        new (&ptr) std::shared_ptr<T>(other.ptr);
+        v = other.v;
         return *this;
-    }
-
-    ~Data() {
-        if (which == 0) {
-            val.~T();
-        } else if (which == 1) {
-            ptr.~shared_ptr();
-        }
     }
 
     void promote() {
-        if (which != 0) {
+        if (v.which() != 0) {
             return;
         }
-        T tmp_val = std::move(val);
-        val.~T();
-        which = 1;
-        new (&ptr) std::shared_ptr<T>(std::make_shared<T>(std::move(tmp_val)));
-    }
-
-    T& get() {
-        return *this;
-    }
-
-    const T& get() const {
-        return *this;
+        v = std::make_shared<T>(std::move(v.template get_unchecked<T>()));
     }
 
     operator T&() {
-        if (which == 0) {
-            return val;
+        if (v.which() == 0) {
+            return v.template get_unchecked<T>();
         } else {
+            auto& ptr = v.template get_unchecked<std::shared_ptr<T>>();
             tlassert(ptr != nullptr);
             return *ptr;
         }
     }
 
-    operator const T&() const {
-        T& out = *const_cast<Data<T>*>(this);
-        return out;
-    }
+    operator const T&() const { return *const_cast<Data<T>*>(this); }
 
-    void save(cereal::BinaryOutputArchive& ar) const {
-        ar(get());
-    }
+    T& get() { return *this; }
+    const T& get() const { return *this; }
 
-    void load(cereal::BinaryInputArchive& ar) {
-        ar(val);
-    }
+    void save(cereal::BinaryOutputArchive& ar) const { ar(get()); }
+    void load(cereal::BinaryInputArchive& ar) { ar(get()); }
 };
 
 template <typename T>
