76d35ff9635ae873a599a456305a9f7c3761ff5b
Ben Thompson <t.ben.thompson@gmail.com>
2017-10-28 00:20:52 -0400

    cut out the signals middleman

--- examples/remote_await.py
+++ examples/remote_await.py
@@ -2,13 +2,12 @@ import taskloaf as tsk
 from taskloaf.promise import task, when_all
 
 async def submit():
-    n = await task(lambda: 1000)
+    n = await task(lambda: 10, to = 1)
     pr = when_all([task(lambda x = x: x, to = x % 2) for x in range(n)]).then(sum)
     v = await pr
-    print(v)
     tsk.shutdown()
     tsk.submit_task(1, tsk.shutdown)
     return v
 
 if __name__ == "__main__":
-    print(tsk.cluster(2, submit, runner = tsk.mpirun))
+    print(tsk.cluster(2, submit))
--- taskloaf/promise.py
+++ taskloaf/promise.py
@@ -1,10 +1,15 @@
 import asyncio
 from taskloaf.worker import submit_task, get_service
-from taskloaf.signals import new_id, signal, set_trigger
+from uuid import uuid1 as uuid
+
+def new_id(addr):
+    return uuid(addr)
 
 def complete_task(S, result):
-    get_service('data')[S] = result
-    signal(S)
+    wf = get_service('waiting_futures')
+    if S not in wf:
+        wf[S] = asyncio.Future()
+    wf[S].set_result(result)
 
 class Promise:
     def __init__(self, owner, signal):
@@ -15,36 +20,28 @@ class Promise:
         here = get_service('comm').addr
         waiting_futures = get_service('waiting_futures')
         if self.signal not in waiting_futures:
-            waiting_futures[self.signal] = []
-        waiting_futures[self.signal].append(asyncio.Future())
+            waiting_futures[self.signal] = asyncio.Future()
+        if here != self.owner:
+            async def wait_for_promise():
+                v = await self
+                submit_task(here, lambda: get_service('waiting_futures')[self.signal].set_result(v))
+            submit_task(self.owner, wait_for_promise)
+        return waiting_futures[self.signal].__await__()
 
-        def fill_futures(x):
-            for fut in get_service('waiting_futures')[self.signal]:
-                fut.set_result(x)
-        self.then(fill_futures, to = here)
-        return waiting_futures[self.signal][-1].__await__()
-
-    def then(self, f, to = None, debug = False):
+    def then(self, f, to = None):
         if to is None:
             to = self.owner
-
-        if debug:
-            print(self.owner, self.signal)
         S = new_id(to)
-        submit_task(
-            self.owner,
-            lambda: set_trigger(self.signal,
-                lambda: task(
-                    lambda D=get_service('data')[self.signal]: f(D),
-                    to = to,
-                    S = S
-                )
-            )
-        )
+
+        async def wait_to_start():
+            v = await self
+            task(lambda v=v: f(v), to = to, S = S)
+        submit_task(self.owner, wait_to_start)
+
         return Promise(to, S)
 
-    def next(self, f, *args, **kwargs):
-        return self.then(lambda x: f(), *args, **kwargs)
+    def next(self, f, to = None):
+        return self.then(lambda x: f(), to)
 
 def task(f, to = None, S = None):
     if to is None:
@@ -69,15 +66,10 @@ def when_all(ps, to = None):
 
     S = new_id(to)
     n = len(ps)
-    for i, p in enumerate(ps):
-        def add_to_result(x, i):
-            data_store = get_service('data')
-            if S not in data_store:
-                data_store[S] = [0, [None] * n]
-            data_store[S][0] += 1
-            data_store[S][1][i] = x
-            if data_store[S][0] == n:
-                complete_task(S, data_store[S][1])
-
-        p.then(lambda x, i = i: add_to_result(x, i), to = to)
+    async def wait_for_all():
+        results = []
+        for i, p in enumerate(ps):
+            results.append(await p)
+        complete_task(S, results)
+    submit_task(to, wait_for_all)
     return Promise(to, S)
