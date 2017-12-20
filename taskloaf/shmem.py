09716ff10784b526137cbbec85406670f9b01116
Ben Thompson <t.ben.thompson@gmail.com>
2017-12-19 23:10:31 -0500

    shmem as contextlib.contextmanager, shmem_edit test

--- taskloaf/shmem.py
+++ taskloaf/shmem.py
@@ -1,5 +1,6 @@
 import os
 import mmap
+import contextlib
 
 class Shmem:
     def __init__(self, filename):
@@ -10,20 +11,13 @@ class Shmem:
     def __del__(self):
         os.close(self.fd)
 
-class OwnedShmem:
-    def __init__(self, name, writeable):
-        self.name = name
-        self.writeable = writeable
-
-    def __enter__(self):
-        self.filename = os.path.join('/dev/shm', self.name)
-        with open(self.filename, 'wb') as f:
-            f.write(self.writeable)
-            f.seek(0, 2)
-            self.size = f.tell()
-        del self.writeable
-        return Shmem(self.filename)
-
-    def __exit__(self, *args):
-        os.remove(self.filename)
-        pass
+@contextlib.contextmanager
+def alloc_shmem(name, writeable):
+    filename = os.path.join('/dev/shm', name)
+    try:
+        with open(filename, 'wb') as f:
+            f.write(writeable)
+        del writeable
+        yield Shmem(filename)
+    finally:
+        os.remove(filename)
--- tests/test_shmem.py
+++ tests/test_shmem.py
@@ -2,9 +2,8 @@ import time
 import numpy as np
 import multiprocessing
 
-from taskloaf.shmem import OwnedShmem, Shmem
+from taskloaf.shmem import alloc_shmem, Shmem
 from taskloaf.timer import Timer
-
 
 def sum_shmem(sm):
     return np.sum(np.frombuffer(sm.mem))
@@ -13,17 +12,24 @@ def shmem_read(filename):
     return sum_shmem(Shmem(filename))
 
 def test_shmem():
-    with OwnedShmem('block', np.random.rand(100)) as sm:
+    with alloc_shmem('block', np.random.rand(100)) as sm:
         out = multiprocessing.Pool(1).map(shmem_read, [sm.filename])[0]
         assert(out == sum_shmem(sm))
 
+def shmem_zeros(filename):
+    np.frombuffer(Shmem(filename).mem)[:] = 0
+
+def test_shmem_edit():
+    with alloc_shmem('block', np.random.rand(100)) as sm:
+        out = multiprocessing.Pool(1).map(shmem_zeros, [sm.filename])[0]
+        np.testing.assert_almost_equal(np.frombuffer(sm.mem), 0.0)
 
 def benchmark_shmem():
     A = np.random.rand(int(3e7)) - 0.5
     print('sum', np.sum(A))
 
     t = Timer()
-    with OwnedShmem('block', memoryview(A)) as sm:
+    with alloc_shmem('block', memoryview(A)) as sm:
         t.report('write file')
         print('sum2', np.sum(np.frombuffer(sm.mem)))
         t.report('read and sum')
