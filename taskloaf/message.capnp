36a53e2df7a0a88998511a9c31d7e26516688e3f
Ben Thompson <t.ben.thompson@gmail.com>
2017-12-27 17:53:41 -0500

    experimenting with general message format.

--- taskloaf/message.capnp
+++ taskloaf/message.capnp
@@ -7,14 +7,31 @@ struct DecRef {
     nchildren @3 :UInt32;
 }
 
-struct Memory {
+struct DistributedRef {
     owner @0 :UInt32;
     creator @1 :UInt32;
     id @2 :Int64;
     gen @3 :UInt32;
-    shmem_ptr @4 :UInt64;
+}
+
+struct TaskRef {
+    dref @0 :DistributedRef;
+}
+
+struct MemoryRef {
+    dref @0 :DistributedRef;
+    shmemPtr @1 :UInt64;
+    valIncluded @2 :Bool;
+    val @3 :Data;
+}
+
+struct Ref {
+    union {
+        memoryRef @0 :MemoryRef;
+        taskRef @1 :TaskRef;
+    }
 }
 
 struct Message {
-    chunks @0 :List(Memory);
+    chunks @0 :List(Ref);
 }
--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -19,31 +19,39 @@ def test_work():
     p.build_work(0, [set])()
     assert(set.val == 1)
 
+
+
 import capnp
+
 import taskloaf.message_capnp
-
-# TODO: Data transfer should use a pull mechanism. (vs push)
-# Why pull? Pull is nice because then the sending worker doesn't need to know what data is available on the receiving worker. Pull does increase latency, but not by a huge amount since it only requires one extra round-trip. Assuming we're in a single data center, that should only be a few microseconds if the software/hardware are properly optimized. I would much rather than tackle the problem of doing extra round-trip messages quickly than the much harder problem of sharing state between the various worker across the system.
-# Implementing a pull mechanism is easy. You just send a message to the owner of a data block asking for the relevant data. Then the worker at the other end of that request replies with the data you want. Within a node, a pull can be done without any communication by sharing a pointer to the location in a shared memory (mmap) block that. If I just always share shmem_ptrs, then this is easy.
-# The presence of a shmem_ptr in every message also has an additional benefit in that this enables a remote request to be sent to any worker on a given node since they can all access the memory chunk. Theoretically interesting, but I doubt it'd be good to take advantage of. There's no way to know which workers on the node are less busy and it would be easy to accidentally incur some big NUMA costs.
-
-
 from taskloaf.memory import Ref
 from taskloaf.promise import encode_ref
 import taskloaf.serialize
 from fakes import fake_worker, FakePromise
 
-def build_msg(refs, dest):
-    # If the destination is
+# TODO: I think I should add the shmemPtr to the ref itself?
+# TODO: The situation with a task launch where the creator != owner leads to some difficulties. This is a weird semi-initialized state. It needs to be somewhat similar to a normal promise in that it should be await-able.
 
-def test_message():
+def send_message(refs):
+    m = taskloaf.message_capnp.Message.new_message()
+    m.init('chunks', len(refs))
+    for i in range(len(refs)):
+        encode_ref(m.chunks[i], refs[i])
+        # m.chunks[i].shmemPtr =
+
+
+def test_build_message():
     worker = fake_worker()
     ref = Ref(worker, worker.addr)
 
     m = taskloaf.message_capnp.Message.new_message()
+    import ipdb
+    ipdb.set_trace()
     m.init('chunks', 1)
-    encode_promise(m.chunks[0].ref, ref)
-    m.chunks[0].shmem_ptr = 0
-    m.chunks[0].included = True
+    encode_ref(m.chunks[0], ref)
+    m.chunks[0].shmemPtr = 0
+    m.chunks[0].valIncluded = True
     m.chunks[0].val = taskloaf.serialize.dumps(lambda x: x * 3)
-    m.to_bytes()
+
+    m_in = taskloaf.message_capnp.Message.from_bytes(m.to_bytes())
+    assert(taskloaf.serialize.loads(None, m_in.chunks[0].val)(9) == 27)
